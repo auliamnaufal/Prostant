a2c3335eb36ab97898c3ab237c59146b66253ce2
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-11-21 14:25:43 +0700

    (feat): display home assignment from javascript

---
+++ scripts/data/Data.js
@@ -0,0 +1,22 @@
+const homeAssignmentData = [
+	{
+		"title": "Membuat Cerita Pendek Tentang Menjaga Kebersamaan",
+		"mapel": "Bahasa Indonesia",
+		"mapelClass": "bindo"
+	},
+	{
+		"title": "Menghitung Luas Bangun Datar",
+		"mapel": "Matematika",
+		"mapelClass": "matematika"
+	},
+	{
+		"title": "Membuat Laporan Penelitian Tentang Kupu-Kupu",
+		"mapel": "Biologi",
+		"mapelClass": "biologi"
+	},
+	{
+		"title": "Bentuk-Bentuk Persamaan Logaritma",
+		"mapel": "Matematika",
+		"mapelClass": "matematika"
+	}
+]
--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,17 @@
-import './components/calender.js'
-import './components/statistic.js'
+const assignmentListElement = document.querySelector(".assignment_list")
+
+homeAssignmentData.forEach(as => {
+	assignmentListElement.innerHTML += `
+		<div class="assignment_item ${as.mapelClass}">
+			<div class="assignment_item_header">
+				<h3 class="assignment_item_title">
+					${as.title}
+				</h3>
+				<p class="assignment_item_subject">${as.mapel}</p>
+			</div>
+			<div class="assignment_detail">
+				<a href="#" class="see_detail">See Assignment</a>
+			</div>
+		</div>
+	`
+})
