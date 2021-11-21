479a1cd61968a2ac2ba681d8ae13fd5db527f31b
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-11-21 14:31:03 +0700

    (refactor): changed name for data file and looping parameter

--- scripts/data/Data.js
+++ scripts/data/Data.js
@@ -1,22 +1,22 @@
 const homeAssignmentData = [
 	{
 		"title": "Membuat Cerita Pendek Tentang Menjaga Kebersamaan",
-		"mapel": "Bahasa Indonesia",
-		"mapelClass": "bindo"
+		"subject": "Bahasa Indonesia",
+		"subjectClass": "bindo"
 	},
 	{
 		"title": "Menghitung Luas Bangun Datar",
-		"mapel": "Matematika",
-		"mapelClass": "matematika"
+		"subject": "Matematika",
+		"subjectClass": "matematika"
 	},
 	{
 		"title": "Membuat Laporan Penelitian Tentang Kupu-Kupu",
-		"mapel": "Biologi",
-		"mapelClass": "biologi"
+		"subject": "Biologi",
+		"subjectClass": "biologi"
 	},
 	{
 		"title": "Bentuk-Bentuk Persamaan Logaritma",
-		"mapel": "Matematika",
-		"mapelClass": "matematika"
+		"subject": "Matematika",
+		"subjectClass": "matematika"
 	}
 ]
--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const assignmentListElement = document.querySelector(".assignment_list")
 
-homeAssignmentData.forEach(as => {
+homeAssignmentData.forEach(assignment => {
 	assignmentListElement.innerHTML += `
-		<div class="assignment_item ${as.mapelClass}">
+		<div class="assignment_item ${assignment.subjectClass}">
 			<div class="assignment_item_header">
 				<h3 class="assignment_item_title">
-					${as.title}
+					${assignment.title}
 				</h3>
-				<p class="assignment_item_subject">${as.mapel}</p>
+				<p class="assignment_item_subject">${assignment.subject}</p>
 			</div>
 			<div class="assignment_detail">
 				<a href="#" class="see_detail">See Assignment</a>
